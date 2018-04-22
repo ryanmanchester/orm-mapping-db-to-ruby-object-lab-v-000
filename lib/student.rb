2485632caf3b17251fdfd345c3c5046b743f1d36
Ryan Manchester <ryanm17@gmail.com>
2018-04-22 20:45:17 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,7 +44,7 @@ class Student
   def self.first_X_students_in_grade_10(num_of_students)
     sql = <<-SQL
     SELECT * FROM students
-    WHERE id = ?
+    WHERE id <= ?
     SQL
     DB[:conn].execute(sql, num_of_students)
   end
