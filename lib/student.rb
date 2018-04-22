5fe73723d9805c23d3a3f487dc5da95f90e9fc18
Ryan Manchester <ryanm17@gmail.com>
2018-04-22 20:39:35 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,9 +29,16 @@ class Student
     SELECT * FROM students
     WHERE grade = 9
     SQL
-    
-    DB[:conn].execute(sql)
 
+    DB[:conn].execute(sql)
+  end
+
+  def self.students_below_12th_grade
+    sql = <<-SQL
+    SELECT * FROM students
+    WHERE grade <= 11
+    SQL
+    DB[:conn].execute(sql)
   end
 
   def save
