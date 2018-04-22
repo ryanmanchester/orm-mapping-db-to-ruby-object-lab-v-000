cb995bb18f8976ba87105c05659831a3ab90b169
Ryan Manchester <ryanm17@gmail.com>
2018-04-22 20:36:40 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -26,8 +26,7 @@ class Student
 
   def self.count_all_students_in_grade_9
     sql = <<-SQL
-    SELECT *
-    FROM students
+    SELECT * FROM students
     WHERE grade = ?
     SQL
     binding.pry
