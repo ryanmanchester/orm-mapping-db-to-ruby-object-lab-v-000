1bcc776161528cfd5ee14450488c48d380e78a8e
Ryan Manchester <ryanm17@gmail.com>
2018-04-22 20:57:35 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -54,7 +54,7 @@ class Student
     SELECT * FROM students
     WHERE grade = 10
     SQL
-    DB[:conn].execute(sql).first
+    DB[:conn].execute(sql).map {|row| self.new_from_db(row)}.first
   end
 
   def self.all_students_in_grade_X(grade)
