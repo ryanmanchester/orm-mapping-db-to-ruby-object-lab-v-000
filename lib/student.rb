24adc9981652e9154507ab8b059b69ad046f2da2
Ryan Manchester <ryanm17@gmail.com>
2018-04-22 20:26:57 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -20,7 +20,7 @@ class Student
     WHERE name = ?
     LIMIT 1
     SQL
-    DB[:conn].execute(sql, self.name).map {|row| self.new_from_db(row)}
+    DB[:conn].execute(sql, name).map {|row| self.new_from_db(row)}
   end
 
   def save
